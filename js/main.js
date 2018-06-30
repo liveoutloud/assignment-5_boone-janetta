6359724c97eb833d774357a4931dbcf626b3636e
Janetta Boone <j.boone@ufl.edu>
2018-06-30 18:37:45 -0500

    step 4 incremental clicks

--- js/main.js
+++ js/main.js
@@ -1,18 +1,8 @@
-//My first commented-out idea creates a new <p> like the step 4 directions say, but
-//I can't figure out how to make 2, 3, etc happen.
+var header = document.getElementById('firstHeading');
+  header.addEventListener("click", function() {
+    var paragraph = document.createElement('p');
+    paragraph.innerHTML = 'This is click number ' +(++clicks);
+    document.getElementById('firstHeading').appendChild(paragraph);
+});
 
-//var firstHeading = document.getElementById( 'firstHeading' );
-//  firstHeading.addEventListener( 'click', function() {
-//  firstHeading.innerHTML = '<p>This is click number 1</p>';
-//});
-
-
-//My second commented-out idea creates a new line of text for each click,
-//but I can't figure out how to change the text to say click 2, click 3, etc.
-
-//function myFunction() {
-//   var hOne = document.createElement("H1");
-//   var one = document.createTextNode("This is click number 1");
-//   hOne.appendChild(one);
-//   document.body.appendChild(hOne);
-//}
+var clicks = 0;
